d8367e7f9a25387c405e79b9b9940fc002203f12
Adem Bilican <bilicanadem@hotmail.com>
2019-09-15 16:00:35 +0300

    console app for client

--- src/ChatService.Server/Program.cs
+++ src/ChatService.Server/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using ChatService.Base;
+using System;
 
 namespace ChatService.Server
 {
     class Program
     {
+        public static ISocketProxy socketProxy;
+        public static ServerBase server;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Server";
+
+            //dependency injection can be use for inject.
+            socketProxy = new SocketProxy();
+            server = new ServerBase(socketProxy);
+
+            server.Run();
+            if (server.IsBoundedServer)
+            {
+                server.BeginListen();
+            }
+
+            Console.ReadLine();
         }
     }
 }
